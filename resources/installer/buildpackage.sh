934a57345bed7f972702c344dd5825a2f3961154
Dinarde Bezerra <dinarde@gmail.com>
2021-08-18 16:24:34 -0300

    2.0.1

--- resources/installer/buildpackage.sh
+++ resources/installer/buildpackage.sh
@@ -1,1 +1,1 @@
-pkgbuild --root ROOT --identifier br.jus.trf2.assijus.chromeextension --version 2.0.0 assijus-chrome-extension-setup.pkg
+pkgbuild --root ROOT --identifier br.jus.trf2.assijus.chromeextension --version 2.0.1 assijus-chrome-extension-setup.pkg
